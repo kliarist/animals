0d572f8d6039af35e7058b7c5cc709f4ff18302e
Theo Kliaris <theodoros.kliaris@gmail.com>
2023-05-15 06:34:14 +0300

    feat: add validation and update postman collection

--- src/dto.rs
+++ src/dto.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use validator::{Validate};
 
 #[derive(Serialize, Debug)]
 pub struct ApiError {
@@ -6,11 +7,15 @@ pub struct ApiError {
     pub errors: Vec<String>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Validate)]
 pub struct AnimalRequestDto {
+    #[validate(length(min = 3, message = "Species name must be at least 3 characters long"))]
     pub species: String,
+    #[validate(length(min = 3, message = "Common name must be at least 3 characters long"))]
     pub common_name: String,
+    #[validate(length(min = 3, message = "Habitat name must be at least 3 characters long"))]
     pub habitat: String,
+    #[validate(range(min = 1, max = 100, message = "Lifespan must be between 1 and 100 years"))]
     pub lifespan: i8,
     pub is_endangered: bool
 }
