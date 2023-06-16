69fe976e666db9c9d9db3cfc5c63b29614682390
Theo Kliaris <theodoros.kliaris@gmail.com>
2023-06-16 23:47:45 +0300

    chore: rename  to app_state

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,3 +1,4 @@
+use crate::app_config::AppConfig;
 use crate::db_util::establish_connection;
 
 use crate::repository::db_animal_repository::DbAnimalRepository;
@@ -8,8 +9,8 @@ pub struct AppState {
 }
 
 impl AppState {
-    pub fn new() -> Self {
-        let db_pool = establish_connection();
+    pub fn new(app_config: &AppConfig) -> Self {
+        let db_pool = establish_connection(&app_config.database_url);
         let animal_repository = DbAnimalRepository::new(db_pool.clone());
 
         Self {
